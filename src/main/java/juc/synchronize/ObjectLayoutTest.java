dd59a9177028037e8cdf4cadf555c908c3d09142
alone_global <alone_global@qq.com>
2020-03-27 22:37:53 +0800

    commit

--- src/main/java/juc/synchronize/ObjectLayoutTest.java
+++ src/main/java/juc/synchronize/ObjectLayoutTest.java
@@ -21,6 +21,15 @@ public class ObjectLayoutTest {
          */
         System.out.println(ClassLayout.parseInstance(object).toPrintable());
         System.out.println("-----------------------------------------------------");
+        /**
+         * 偏向锁: 第一个对象来获取锁,给markword的线程指针上加上已经获得锁的线程地址,不需要向操作系统获取重量级锁
+         * 轻量级锁(自旋锁,无锁):撤销偏向锁,多个线程抢锁,CAS操作,
+         *          谁能将指针贴在对象的地址上就获得了锁(指向线程的Lock Record指针)
+         * 重量级锁:一直自旋,升级为重量级锁(由用户态调用系统调用向核心态申请)
+         *
+         * 锁消除: 多次加锁和解锁的情况下,没有其他线程访问,jvm就会自动将锁消除
+         * 锁粗化: 一连串的对一个对象加锁和解锁,jvm就会将锁放在这一连串外面
+         */
         synchronized (object) {
             /*
                 java.lang.Object object internals:
